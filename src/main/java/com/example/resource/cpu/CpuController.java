9dba2672611f0e89563cf4580569b207356c6920
glass <silver20190204@gmail.com>
2024-10-21 23:22:21 +0900

    refactor: 반환 타입 변경

--- src/main/java/com/example/resource/cpu/CpuController.java
+++ src/main/java/com/example/resource/cpu/CpuController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("cpu")
@@ -16,19 +18,23 @@ public class CpuController {
     private static final String PROC_STAT = "/proc/stat";
 
     @GetMapping("/v1/stat")
-    public ResponseEntity<String> getCpuStat(){
-        var result = new StringBuilder();
+    public ResponseEntity<Map<String, String>> getCpuStat(){
+        var result = new HashMap<String, String>();
         try (var reader = new BufferedReader(new FileReader(PROC_STAT))) {
             for (String line = reader.readLine(); line != null; line = reader.readLine()) {
                 if (!line.startsWith("cpu")) {
                     break;
                 }
-                result.append(line);
+                String[] tokens = line.split("\\s+", 2);
+                if (tokens.length >= 2) {
+                    result.put(tokens[0], tokens[1]);
+                }
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error reading CPU stats: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                 .body(Map.of("error", "Error reading CPU stats: " + e.getMessage()));
         }
-        return ResponseEntity.ok(result.toString());
+        return ResponseEntity.ok(result);
     }
 }
