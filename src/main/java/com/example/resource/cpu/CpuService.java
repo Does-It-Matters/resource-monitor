08a2dd51466f1a0f088bd8270b4c492996464882
glass <silver20190204@gmail.com>
2024-10-22 01:01:45 +0900

    refactor: 도메인 모델로 분리

--- src/main/java/com/example/resource/cpu/CpuService.java
+++ src/main/java/com/example/resource/cpu/CpuService.java
@@ -1,27 +1,17 @@
 package com.example.resource.cpu;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.util.*;
 
+@RequiredArgsConstructor
 @Service
 public class CpuService {
-    private static final String PROC_STAT = "/proc/stat";
+    private final CpuStat cpuStat;
 
     public Map<String, String> getStat() throws IOException {
-        var reader = new BufferedReader(new FileReader(PROC_STAT));
-        var cpuStat = new HashMap<String, String>();
-        for (String line = reader.readLine(); startWith(line, "cpu"); line = reader.readLine()) {
-            String[] tokens = line.split("\\s+", 2);
-            if (tokens.length >= 2) {
-                cpuStat.put(tokens[0], tokens[1]);
-            }
-        }
-        return cpuStat;
-    }
-
-    private boolean startWith(String line, String word) {
-        return line != null && line.startsWith(word);
+        return cpuStat.getStat("cpu");
     }
 }
